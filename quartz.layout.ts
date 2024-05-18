0f944c2a9b84a1730925c7e08ffd85629254de52
Ellen Su <86137909+eysu35@users.noreply.github.com>
2024-05-18 14:30:58 -0400

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,7 +31,8 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    // Component.Backlinks(),
+    Component.TableOfContents(),
   ],
 }
 
