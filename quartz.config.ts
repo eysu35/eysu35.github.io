7ac00a5200a4f98d171f6a1b9f4b00b95f6ef2fd
Ellen Su <86137909+eysu35@users.noreply.github.com>
2024-07-04 18:02:16 -0400

    title again

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Hi, I'm Ellen!",
+    pageTitle: "Ellen Su",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
