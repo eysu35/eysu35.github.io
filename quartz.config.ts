8f5c7b9c3838c867590629fcb99fdc08bebc9812
Ellen Su <86137909+eysu35@users.noreply.github.com>
2024-07-04 20:20:17 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Baskervville SC",
+        header: "Cormorant",
         body: "IBM Plex Sans",
         code: "IBM Plex Mono",
       },
