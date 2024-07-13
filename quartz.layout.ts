726b1f1acbcfe1fbe2808d6c7aaa396636a08e2c
Ellen Su <86137909+eysu35@users.noreply.github.com>
2024-07-13 18:32:25 -0400

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: Component.Header(),
+  header: [Component.Header()],
   footer: Component.Footer({
     links: {
       "GitHub": "https://github.com/eysu35/",
