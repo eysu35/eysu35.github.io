2f12f988fab4fbca0e97a8e9e5ee104965e691c6
eysu35 <ellensu1230@gmail.com>
2025-04-13 10:54:46 -0400

    Quartz sync: Apr 13, 2025, 10:54 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,6 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [Component.TopNav()],
   footer: Component.Footer({
     links: {
+      "Google Scholar": "https://scholar.google.com/citations?hl=en&user=Tzp43qcAAAAJ",
       "GitHub": "https://github.com/eysu35/",
       "LinkedIn": "https://www.linkedin.com/in/ellen-su-4384221a6/",
     },
